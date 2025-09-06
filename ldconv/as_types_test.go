499902bd130a55d54c3635ac1d80552e06d440f0
distroy <distroy@live.com>
2025-09-06 21:43:18 +0800

    feat(ldconv): add

---
+++ ldconv/as_types_test.go
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) distroy
+ */
+
+package ldconv
+
+import (
+	"testing"
+
+	"github.com/distroy/ldgo-base/3rd/convey"
+)
+
+func TestAsBool(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(AsBool("abc"), convey.ShouldEqual, false)
+		convey.So(AsBool("abc", true), convey.ShouldEqual, true)
+
+		convey.So(AsBool("true", false), convey.ShouldEqual, true)
+		convey.So(AsBool("false", true), convey.ShouldEqual, false)
+		convey.So(AsBool([]byte("null"), true), convey.ShouldEqual, false)
+
+		convey.So(AsBool(0, true), convey.ShouldEqual, false)
+
+		convey.So(AsBool(int(-1)), convey.ShouldEqual, true)
+		convey.So(AsBool(uint(1)), convey.ShouldEqual, true)
+	})
+}
+
+func TestAsInt(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(AsInt("abc"), convey.ShouldEqual, 0)
+		convey.So(AsInt("abc", 1), convey.ShouldEqual, 1)
+
+		convey.So(AsInt("true", 0), convey.ShouldEqual, 1)
+		convey.So(AsInt("false", 1), convey.ShouldEqual, 0)
+		convey.So(AsInt([]byte("null"), 1), convey.ShouldEqual, 0)
+
+		convey.So(AsInt(0, 100), convey.ShouldEqual, 0)
+
+		convey.So(AsInt(int(-1)), convey.ShouldEqual, -1)
+		convey.So(AsInt(uint(1)), convey.ShouldEqual, 1)
+
+		// convey.So(AsInt(mustNewDecimalFromStr("-123.234")), convey.ShouldEqual, -123)
+	})
+}
+
+func TestAsFloat32(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(AsFloat32("0"), convey.ShouldEqual, 0)
+		convey.So(AsFloat32("abc"), convey.ShouldEqual, 0)
+		convey.So(AsFloat32("abc", 1), convey.ShouldEqual, 1)
+
+		convey.So(AsFloat32("true", 0), convey.ShouldEqual, 1)
+		convey.So(AsFloat32("false", 1), convey.ShouldEqual, 0)
+		convey.So(AsFloat32([]byte("null"), 1), convey.ShouldEqual, 0)
+
+		convey.So(AsFloat32(0, 100), convey.ShouldEqual, 0)
+
+		convey.So(AsFloat32(int(-1)), convey.ShouldEqual, -1)
+		convey.So(AsFloat32(uint(1)), convey.ShouldEqual, 1)
+
+		convey.So(AsFloat32("-123.234"), convey.ShouldEqual, float32(-123.234))
+		// convey.So(AsFloat32(mustNewDecimalFromStr("-123.234")), convey.ShouldEqual, float32(-123.234))
+	})
+}
+
+func TestAsFloat64(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(AsFloat64("0"), convey.ShouldEqual, 0)
+		convey.So(AsFloat64("abc"), convey.ShouldEqual, 0)
+		convey.So(AsFloat64("abc", 1), convey.ShouldEqual, 1)
+
+		convey.So(AsFloat64("true", 0), convey.ShouldEqual, 1)
+		convey.So(AsFloat64("false", 1), convey.ShouldEqual, 0)
+		convey.So(AsFloat64([]byte("null"), 1), convey.ShouldEqual, 0)
+
+		convey.So(AsFloat64(0, 100), convey.ShouldEqual, 0)
+
+		convey.So(AsFloat64(int(-1)), convey.ShouldEqual, -1)
+		convey.So(AsFloat64(uint(1)), convey.ShouldEqual, 1)
+
+		// convey.So(AsFloat64(mustNewDecimalFromStr("-123.234")), convey.ShouldEqual, -123.234)
+	})
+}
---
+++ ldconv/conv_number_test.go
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) distroy
+ */
+
+package ldconv
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/distroy/ldgo-base/3rd/convey"
+)
+
+const (
+	_TEST_FLOAT_MUL_BASE = 100000
+)
+
+func testConvInt(s string, r int64) {
+	convey.Convey(s, func() {
+		convey.So(AsInt64(s), convey.ShouldEqual, r)
+	})
+}
+
+func testConvUint(s string, r uint64) {
+	convey.Convey(s, func() {
+		convey.So(AsUint64(s), convey.ShouldEqual, r)
+	})
+}
+
+func testConvFloat(s string, rf32 float32, rf64 float64, ri64 int64) {
+	name := fmt.Sprintf("%s|%g|%g|%d", s, rf32, rf64, ri64)
+	convey.Convey(name, func() {
+
+		convey.So(AsFloat32(s), convey.ShouldEqual, rf32)
+
+		convey.So(AsFloat64(s), convey.ShouldEqual, rf64)
+
+		n0 := asDecimal(s)
+		baseFloat := asDecimal(_TEST_FLOAT_MUL_BASE)
+		n1 := n0.Mul(baseFloat)
+		vi64, _ := n1.BigFloat().Int64()
+		convey.So(vi64, convey.ShouldEqual, ri64)
+	})
+}
+
+func TestConvInt(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		testConvInt("0", 0)
+		testConvInt("123", 123)
+		testConvInt("123.1", 123)
+		testConvInt(".1", 0)
+		testConvInt("0123", 123)
+		testConvInt("0o123", 0123)
+		testConvInt("0O123", 0123)
+		testConvInt("+-0o123", -0123)
+		testConvInt("123e+1", 1230)
+		testConvInt("123e1", 1230)
+		testConvInt("123e-1", 12)
+		testConvInt("0x123", 0x123)
+		testConvInt("--0X123", 0x123)
+		testConvInt("-+-0x123", 0x123)
+		testConvInt("-+-0xeFaB", 0xeFaB)
+	})
+}
+
+func TestConvUint(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		testConvUint("0", 0)
+		testConvUint("123", 123)
+		testConvUint("123.1", 123)
+		testConvUint(".1", 0)
+		testConvUint("0123", 123)
+		testConvUint("0o123", 0123)
+		// testConvUint("+-0123", uint64(int64(-0123)))
+		testConvUint("123e+1", 1230)
+		testConvUint("123e1", 1230)
+		testConvUint("123e-1", 12)
+		testConvUint("0x123", 0x123)
+		testConvUint("--0X123", 0x123)
+		testConvUint("-+-0x123", 0x123)
+		testConvUint("-+-0xeFaB", 0xeFaB)
+	})
+}
+
+func TestConvFloat(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		testConvFloat("0", 0, 0, 0*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("9.9", 9.9, 9.9, 9.9*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("20.09", 20.09, 20.09, 20.09*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("19.99", 19.99, 19.99, 19.99*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0.99", 0.99, 0.99, 0.99*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("2.32", 2.32, 2.32, 2.32*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("123", 123, 123, 123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("123.1", 123.1, 123.1, 123.1*_TEST_FLOAT_MUL_BASE)
+		testConvFloat(".1", 0.1, 0.1, 0.1*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0123", 123, 123, 123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0o123", 0123, 0123, 0123*_TEST_FLOAT_MUL_BASE)
+		// testConvFloat("+-0123", uint64(int64(-0123)))
+		testConvFloat("123e+1", 1230, 1230, 1230*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("123e1", 1230, 1230, 1230*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("123e-1", 12.3, 12.3, 12.3*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0x123", 0x123, 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("--0X123", 0x123, 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("-+-0x123", 0x123, 0x123, 0x123*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("-+-0xeFaB", 0xeFaB, 0xeFaB, 0xeFaB*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("---0xeFaB", -0xeFaB, -0xeFaB, -0xeFaB*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0.30129", 0.30129, 0.30129, 0.30129*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("0.30129e3", 301.29, 301.29, 301.29*_TEST_FLOAT_MUL_BASE)
+		testConvFloat("-0.30129E-3", -0.00030129, -0.00030129, -30)
+	})
+}
